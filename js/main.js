46212c28d8a991a69fba846554736f9fa8b9dc00
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-07-28 22:01:44 +0000

    Added view swapping functionality to program. If new tabs are created in the future they will be easier switch views with. Deleted unnecessary code. Created a new attribute data-view and matched it with necessary elements for the view-switching function to work.

--- js/main.js
+++ js/main.js
@@ -1,48 +1,33 @@
 
 var $addCard = document.querySelector('#add-card');
-var $searchBarDiv = document.querySelector('#search-bar');
-var $cardCount = document.querySelector('#card-deck-count');
-var $infoText = document.querySelector('#click-to-add');
 var $noDeck = document.querySelector('#no-deck');
-var $newDeck = document.querySelector('.new-deck');
 var $search = document.querySelector('#search');
 var $myDeck = document.querySelector('#my-deck');
-var $myDeckView = document.querySelector('#my-deck-view');
 var $logo = document.querySelector('.logo');
 var $searchBar = document.querySelector('.search-bar');
+var $tabView = document.querySelectorAll('.tab-view');
 
-function searchBarAppears(event) {
-  event.preventDefault();
-  $newDeck.className = 'new-deck hidden';
-  $cardCount.className = 'minor-padding column-full row space-between hidden';
-  $searchBarDiv.className = 'colun-full row justify-center search-bar-padding';
-  $infoText.className = 'row justify-center align-center hidden';
-  $myDeckView.className = 'row justify-center align-center';
+function switchView(dataView) {
+  for (var i = 0; i < $tabView.length; i++) {
+    if ($tabView[i].getAttribute('data-view') === dataView) {
+      $tabView[i].className = 'tab-view';
+    } else {
+      $tabView[i].className = 'tab-view hidden';
+    }
+  }
 }
 
-function myDeckAppears(event) {
+function switchViewing(event) {
   event.preventDefault();
-  $newDeck.className = 'new-deck';
-  $cardCount.className = 'minor-padding column-full row space-between hidden';
-  $searchBarDiv.className = 'colun-full row justify-center hidden';
-  $infoText.className = 'row justify-center align-center hidden';
-  $myDeckView.className = 'row justify-center align-center';
+  var dataView = event.target.getAttribute('data-view');
+  switchView(dataView);
 }
 
-function newDeck(event) {
-  event.preventDefault();
-  $newDeck.className = 'new-deck hidden';
-  $cardCount.className = 'minor-padding column-full row space-between';
-  $searchBarDiv.className = 'colun-full row justify-center hidden';
-  $infoText.className = 'row justify-center align-center';
-  $myDeckView.className = 'row justify-center align-center';
-}
-
-$myDeck.addEventListener('click', myDeckAppears);
-$search.addEventListener('click', searchBarAppears);
-$noDeck.addEventListener('click', newDeck);
-$addCard.addEventListener('click', searchBarAppears);
-$logo.addEventListener('click', myDeckAppears);
+$myDeck.addEventListener('click', switchViewing);
+$search.addEventListener('click', switchViewing);
+$noDeck.addEventListener('click', switchViewing);
+$addCard.addEventListener('click', switchViewing);
+$logo.addEventListener('click', switchViewing);
 
 function search(inputValue) {
   var yugiohIndex = new XMLHttpRequest();
